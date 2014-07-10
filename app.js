5505b8ba1ae694716c1609438eeddd08951d64c7
basseljaafar <basseljaafar@gmail.com>
2014-07-09 20:46:29 -0500

    added-Questions

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+//$(document).ready(function(){
 
 function Question(quest, one, two, three, four, correct){
 	this.quest = quest;
@@ -9,7 +9,37 @@ function Question(quest, one, two, three, four, correct){
 	this.correct = correct;
 }
 
-var Aquestion = new Question("Who did Roger Federer beat to win his first Grand Slam?", "Marc Philipousis", "David Ferrer", "Andre Agassi", "Juan Carlos Ferrero", "Marc Philipousis");
+var Aquestion = new Question("Who did Roger Federer beat to win his first Grand Slam?","Marc Philipousis","David Ferrer","Andre Agassi","Juan Carlos Ferrero","Marc Philipousis");
+ 
+ var Bquestion = new Question("Who of these players was the youngest to hold the number 1 ranking?",
+ "Boris Becker",
+ "John McEnroe",
+ "Leyton Hewitt",
+ "Andre Agassi",
+ "Leyton Hewitt");
+ 
+ var Cquestion = new Question("Which of these players won three Grand Slams in 2002",
+ "Martina Hingis",
+ "Kim Clijsters",
+ "Serena Williams",
+ "Justine Henin",
+ "Serena Williams");
+ 
+ var Dquestion = new Question("Who ended up winning the longest tennis match ever played ?",
+ "John Isner",
+ "Tommy Haas",
+ "Tommy Robredo",
+ "Nicolas Mahut",
+ "John Isner");
+ 
+ var arr = [];
+ arr[0] = Aquestion;
+ arr[1] = Bquestion;
+ arr[2] = Cquestion;
+ arr[3] = Dquestion;
+ 
+ console.log(arr);
+ 
 var a = Aquestion.quest;
 $("p.question").text(a);
 $(".one").text(Aquestion.one);
@@ -17,4 +47,4 @@ $(".two").text(Aquestion.two);
 $(".three").text(Aquestion.three);
 $(".four").text(Aquestion.four);
 
-});
+//});
